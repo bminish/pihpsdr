2fa933ca20989431cdd0eeb07de85f65cd50b77a
Brendan Minish <bminish@gmail.com>
2026-07-28 20:35:53 +0100

    style: Clean license header in src/pipewire.c per README_STYLE.md

--- src/pipewire.c
+++ src/pipewire.c
@@ -1,5 +1,5 @@
 /* Copyright (C)
- *  2026 - Antigravity AI Coding Assistant / Google DeepMind
+ *  2026 - Brendan Minish & Antigravity AI Coding Assistant / Google DeepMind
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -11,10 +11,8 @@
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *   GNU General Public License for more details.
  *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
  */
 
